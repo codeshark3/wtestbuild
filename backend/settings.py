96f0373895905a27139caada3e19b02e7a82926f
joel <jobaff03@gmail.com>
2021-09-28 12:12:00 +0000

    next try

--- backend/settings.py
+++ backend/settings.py
@@ -129,17 +129,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR ,'static'),
-    os.path.join(BASE_DIR,'webfrontend/build/static')
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR ,'static'),
+#     os.path.join(BASE_DIR,'webfrontend/build/static')
+# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'webfrontend/build/static'
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ORIGIN_ALLOW_ALL = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
